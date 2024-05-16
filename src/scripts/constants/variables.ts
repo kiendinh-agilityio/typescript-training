a422f93facdb590e1e2f7c811049b7063ba7d4c9
Kien Dinh <kien.dinh@asnet.com.vn>
2024-05-16 14:58:14 +0700

    feat: move from js practice to ts practice and update variables file

---
+++ src/scripts/constants/variables.ts
@@ -0,0 +1,26 @@
+// Endpoints name
+export const END_POINTS = {
+  USERS: 'users',
+  ADS: 'ads',
+};
+
+// Variables class
+export const DISPLAY_CLASS = {
+  FLEX: 'flex',
+  HIDDEN: 'none',
+  BLOCK: 'block',
+  ACTIVE: 'active',
+  DISABLED: 'disabled',
+};
+
+// Varibales for icon
+export const ICONS = {
+  SUCCESS: 'icon-success.svg',
+  ERROR: 'icon-error.svg',
+};
+
+// Auth Form Title
+export const TITLE_AUTH_PAGE = {
+  LOGIN: 'Sign In',
+  REGISTER: 'Create New Account',
+};
