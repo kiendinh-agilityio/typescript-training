bd8573bdb46bc07c45513b0a3d126e84af4dae2d
Kien Dinh <kien.dinh@asnet.com.vn>
2024-05-16 17:46:48 +0700

    feat: add validateConfirmPasswordField for file validate

--- src/scripts/validate/validate.ts
+++ src/scripts/validate/validate.ts
@@ -1,9 +1,17 @@
 import { REGEX, VALIDATE_MESSAGES } from '@/constants';
 
-const { INVALID_EMAIL, REQUIRED_ERROR, INVALID_PASSWORD } = VALIDATE_MESSAGES;
+const { INVALID_EMAIL, REQUIRED_ERROR, INVALID_PASSWORD, INVALID_CONFIRM_PASSWORD } = VALIDATE_MESSAGES;
 
 const validateField = (value: string | undefined, fieldName: string, regex: RegExp, errorMessage: string): string | null =>
   !value ? REQUIRED_ERROR.replace('{field}', fieldName) : (!regex.test(value) ? errorMessage.replace('{field}', fieldName) : null);
 
 export const validateEmailField = (email: string): string | null => validateField(email, 'Email', REGEX.EMAIL, INVALID_EMAIL);
 export const validatePasswordField = (password: string): string | null => validateField(password, 'Password', REGEX.CHARACTERS, INVALID_PASSWORD);
+export const validateConfirmPasswordField = (password: string, confirmPassword: string): string | null => {
+  if (!confirmPassword) {
+    return REQUIRED_ERROR.replace('{field}', 'Confirm Password');
+  } else if (password !== confirmPassword) {
+    return INVALID_CONFIRM_PASSWORD;
+  }
+  return null;
+};
