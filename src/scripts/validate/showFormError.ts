832f4f86ac4285ce9520eedcc79b3127257bf38d
Kien Dinh <kien.dinh@asnet.com.vn>
2024-05-16 15:00:19 +0700

    feat: move from js practice to ts practice and update for showFormError

---
+++ src/scripts/validate/showFormError.ts
@@ -0,0 +1,17 @@
+interface Errors {
+  [key: string]: string | undefined;
+}
+
+const updateErrorMessages = (errors: Errors): void => {
+  Object.entries(errors).forEach(([key, value]) => {
+    const target = document.getElementById(`${key}-error`);
+    if (target) {
+      // Ensure target.innerText is a string
+      target.innerText = value || '';
+    }
+  });
+};
+
+export const showFormErrors = (errors: Errors): void => {
+  updateErrorMessages(errors);
+};
