6be59f6bae60c6db84878596000663fb2ab7c79b
Kien Dinh <kien.dinh@asnet.com.vn>
2024-05-16 14:58:00 +0700

    feat: move from js practice to ts practice and update regex file

---
+++ src/scripts/constants/regex.ts
@@ -0,0 +1,19 @@
+export const REGEX = {
+  // Check email have @ symbol, a string precedes it and the following string needs to contain a period, followed by 2-3 characters.
+  EMAIL: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+
+  // Check password
+  CHARACTERS: /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+
+  // Check regex for link
+  LINK: /^(https?:\/\/)?(www\.)?[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})+(\.[a-zA-Z]{2,})?([/?].*)?$/,
+
+  // Check the character must match the phone number format. Example: (205)-205-5555
+  PHONE: /^\(\d{3}\)-\d{3}-\d{4}$/,
+
+  // Check characters with strings or numbers between 6 and 30 characters in length
+  NETWORK: /[0-9a-zA-Z]{4,20}/,
+
+  // Keyword
+  KEYWORD: /\s/g,
+};
