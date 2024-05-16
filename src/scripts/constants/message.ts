b366eadd6025ef4760b84db272299d24dacc3a95
Kien Dinh <kien.dinh@asnet.com.vn>
2024-05-16 14:57:36 +0700

    feat: move from js practice to ts practice and update message file

---
+++ src/scripts/constants/message.ts
@@ -0,0 +1,13 @@
+// Message
+export const VALIDATE_MESSAGES = {
+  REQUIRED_ERROR: '{field} is required',
+  INVALID_EMAIL: 'Invalid email. Please enter a valid email address',
+  INVALID_PASSWORD: 'Invalid password. Password must have at least 8 characters, including at least one digit, one special character, one lowercase letter, and one uppercase letter.',
+};
+
+// Login
+export const LOGIN_MESSAGES = {
+  SUCCESS: 'Sign in successfully',
+  UNSUCCESSFUL: 'Sign in unsuccessful',
+  EMPTY: 'Please enter both email and password.',
+};
