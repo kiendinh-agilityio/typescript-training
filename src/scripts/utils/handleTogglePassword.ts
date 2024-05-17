b80ef37dd1771cae39fa7786285e79d0001b643d
Kien Dinh <kien.dinh@asnet.com.vn>
2024-05-17 17:40:58 +0700

    feat: update file handleTogglePassword

--- src/scripts/utils/handleTogglePassword.ts
+++ src/scripts/utils/handleTogglePassword.ts
@@ -1,9 +1,12 @@
+// Constants
+import { IMAGE_BASE_PATH } from '@/constants';
+
 export const handleTogglePassword = (togglePasswordButton: HTMLElement) => {
   const inputField = togglePasswordButton.parentElement!.querySelector('.show-password') as HTMLInputElement;
   const eyeImage = togglePasswordButton.querySelector('img') as HTMLImageElement;
   const isShow = inputField.getAttribute('data-show-password') === 'show';
 
-  const eyeImageSrc = isShow ? '/images/svg/eye-key-password.svg' : '/images/svg/eye-password.svg';
+  const eyeImageSrc = isShow ? IMAGE_BASE_PATH.EYE_KEY_PASSWORD : IMAGE_BASE_PATH.EYE_PASSWORD;
   eyeImage.src = eyeImageSrc;
 
   inputField.type = isShow ? 'password' : 'text';
