6628338dacbbb244cb78ada1afe3c1ab6f00031b
Kien Dinh <kien.dinh@asnet.com.vn>
2024-05-17 17:49:13 +0700

    feat: fix conflict

--- src/scripts/constants/message.ts
+++ src/scripts/constants/message.ts
@@ -3,6 +3,7 @@ export const VALIDATE_MESSAGES = {
   REQUIRED_ERROR: '{field} is required',
   INVALID_EMAIL: 'Invalid email. Please enter a valid email address',
   INVALID_PASSWORD: 'Invalid password. Password must have at least 8 characters, including at least one digit, one special character, one lowercase letter, and one uppercase letter.',
+  INVALID_CONFIRM_PASSWORD: 'Password and Confirm Password do not match.',
 };
 
 // Login
@@ -14,3 +15,10 @@ export const LOGIN_MESSAGES = {
 
 // Error Fetching
 export const ERROR_FETCHING_DATA: string = "Error fetching user data";
+
+// Message Signup
+export const SIGNUP_MESSAGES = {
+  SUCCESS: 'Registration successful. You can now sign in.',
+  EMAIL: 'Email already exists. Please use a different email.',
+  EMPTY: 'Email, password, and confirm password are required.',
+};
