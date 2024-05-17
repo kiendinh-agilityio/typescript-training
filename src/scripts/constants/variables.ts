51e21cbbfc6be7713482f938a08f533269d65d10
Kien Dinh <kien.dinh@asnet.com.vn>
2024-05-17 17:39:11 +0700

    feat: add constants image path for variables.ts

--- src/scripts/constants/variables.ts
+++ src/scripts/constants/variables.ts
@@ -24,3 +24,9 @@ export const TITLE_AUTH_PAGE = {
   LOGIN: 'Sign In',
   REGISTER: 'Create New Account',
 };
+
+// Image Path for Show Password
+export const IMAGE_BASE_PATH = {
+  EYE_KEY_PASSWORD: '/images/svg/eye-key-password.svg',
+  EYE_PASSWORD: '/images/svg/eye-password.svg',
+};
