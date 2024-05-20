f6808c1f67dc30f2e23404ae2c885ba848e45def
Kien Dinh <kien.dinh@asnet.com.vn>
2024-05-20 22:36:43 +0700

    feat: apply typeScript for displaying the ads list

--- src/scripts/pages/home.ts
+++ src/scripts/pages/home.ts
@@ -1,3 +1,11 @@
 import { initializeSidebar } from '@/sidebar';
+import { AdsController } from '../controllers/home';
+import { AdsView } from '../views/home';
+import { AdsModel } from '../models/home';
 
+const homePage = () => {
+  new AdsController(new AdsModel(), new AdsView());
+};
+
+homePage();
 initializeSidebar();
