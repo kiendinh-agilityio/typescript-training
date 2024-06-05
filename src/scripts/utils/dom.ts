61d3bf898a7b55dea8936d64adf9978f23829780
Kien Dinh <kien.dinh@asnet.com.vn>
2024-06-05 15:25:49 +0700

    feat: remove ! for authSection and adsSearchElement

--- src/scripts/utils/dom.ts
+++ src/scripts/utils/dom.ts
@@ -1,2 +1,2 @@
-export const authSection: HTMLElement = document.getElementById('authen-section')!;
-export const adsSearchElement: HTMLElement = document.querySelector('.ads-search')!;
+export const authSection: HTMLElement = document.getElementById('authen-section');
+export const adsSearchElement: HTMLElement = document.querySelector('.ads-search');
