c1644f36486def3645f68302e61e850a15b93b94
Kien Dinh <kien.dinh@asnet.com.vn>
2024-06-06 16:15:13 +0700

    feat: update file dom.ts

--- src/scripts/utils/dom.ts
+++ src/scripts/utils/dom.ts
@@ -1,3 +1,12 @@
+// Get the element with ID 'authen-section'
 export const authSection: HTMLElement = document.getElementById('authen-section');
+
+// Get the first element with class 'ads-search'
 export const adsSearchElement: HTMLElement = document.querySelector('.ads-search');
+
+// Get the first element with class 'ads-section'
 export const adsSection: HTMLElement = document.querySelector('.ads-section');
+
+// Get the element with ID 'modal' within adsSection
+export const modalAds: HTMLElement = adsSection.querySelector('#modal');
+export const deleteModal: HTMLElement = adsSection.querySelector('#delete-modal');
