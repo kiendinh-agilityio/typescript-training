2c5afa08e6ad8c6ba30ecc5f765cd6d7d6787881
Kien Dinh <kien.dinh@asnet.com.vn>
2024-06-07 17:10:32 +0700

    feat: update type for element

--- src/scripts/utils/dom.ts
+++ src/scripts/utils/dom.ts
@@ -1,12 +1,20 @@
+/**
+ * @template T - Expected HTML element type.
+ * @param {string} id/selector - ID or CSS selector of the element.
+ * @returns {T} - The HTML element casted to type T.
+ */
+const getElementById = <T extends HTMLElement>(id: string): T =>
+  document.getElementById(id) as T;
+
+const querySelector = <T extends HTMLElement>(selector: string): T =>
+  document.querySelector(selector) as T;
+
 // Get the element with ID 'authen-section'
-export const authSection: HTMLElement = document.getElementById('authen-section');
+export const authSection = getElementById<HTMLDivElement>('authen-section');
 
 // Get the first element with class 'ads-search'
-export const adsSearchElement: HTMLElement = document.querySelector('.ads-search');
+export const adsSearchElement = querySelector<HTMLDivElement>('.ads-search');
 
-// Get the first element with class 'ads-section'
-export const adsSection: HTMLElement = document.querySelector('.ads-section');
-
-// Get the element with ID 'modal' within adsSection
-export const modalAds: HTMLElement = adsSection.querySelector('#modal');
-export const deleteModal: HTMLElement = adsSection.querySelector('#delete-modal');
+// Get the element with ID 'modal'
+export const modalAds = getElementById('modal');
+export const deleteModal = getElementById('delete-modal');
