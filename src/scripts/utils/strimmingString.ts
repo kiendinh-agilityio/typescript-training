23d1b6b56250ea4e53b791e15b64cf0826522d57
Kien Dinh <kien.dinh@asnet.com.vn>
2024-06-19 22:31:10 +0700

    feat: update types for strimmingString file does not use any

--- src/scripts/utils/strimmingString.ts
+++ src/scripts/utils/strimmingString.ts
@@ -1,1 +1,5 @@
-export const strimmingString = (value: string | any): string | any => typeof value === 'string' ? value.trim() : value;
+export const strimmingString = <T>(value: T): T extends string ? string : T => {
+  return (typeof value === 'string' ? value.trim() : value) as T extends string
+    ? string
+    : T;
+};
