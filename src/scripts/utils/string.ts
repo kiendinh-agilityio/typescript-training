41d9f52130aeb8fa0f2549005597986b84c2623c
Kien Dinh <kien.dinh@asnet.com.vn>
2024-07-18 15:19:02 +0700

    feat: create utils string

---
+++ src/scripts/utils/string.ts
@@ -0,0 +1,6 @@
+/**
+ * Trims whitespace from both ends of a given string.
+ * @param value - The string to trim.
+ * @returns The trimmed string.
+ */
+export const trailingString = (value: string): string => value.trim();
