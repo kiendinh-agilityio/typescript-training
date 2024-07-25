bb0ab6ea72def1773eb28d94422a55ed4360f453
Kien Dinh <kien.dinh@asnet.com.vn>
2024-07-25 16:15:40 +0700

    feat: update feature render student to the student list

--- src/scripts/pages/student.ts
+++ src/scripts/pages/student.ts
@@ -2,7 +2,11 @@
 import { PERSONS, END_POINTS } from '@/constants';
 
 // Import utils
-import { showTabletNoData } from '@/utils';
+import {
+  showTabletNoData,
+  startLoadingSpinner,
+  stopLoadingSpinner,
+} from '@/utils';
 
 // Import class StudentList
 import { StudentList } from '@/events';
@@ -27,10 +31,18 @@ export class StudentPage {
    * Initializes and fetches the initial data.
    */
   async initialize(): Promise<void> {
+    // Start the loading spinner to indicate data fetching
+    startLoadingSpinner();
+
+    // Fetch person data from the service
     const data = await this.personServices.fetchPersonData();
 
+    // Check if data is available and has items
     data && data.length > 0
       ? this.studentList.displayStudentList(data)
       : showTabletNoData(PERSONS.STUDENTS);
+
+    // Directly stop loading spinner after response is received
+    stopLoadingSpinner();
   }
 }
