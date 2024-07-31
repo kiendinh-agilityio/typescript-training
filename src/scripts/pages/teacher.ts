46b4d1d4cbba9e2978bb78d2cf120d045f9b0fcf
Kien Dinh <kien.dinh@asnet.com.vn>
2024-07-31 14:59:14 +0700

    feat: upadte feature add and edit for teacher

--- src/scripts/pages/teacher.ts
+++ src/scripts/pages/teacher.ts
@@ -115,26 +115,14 @@ export class TeacherPage {
    * @param {object} newPerson - The data of the new teacher to be added.
    */
   async handleAddTeacher(newPerson: Person): Promise<void> {
-    // Introduce a delay before adding the new teacher
-    delayAction(async () => {
-      // Send a request to add the new person and await the response
-      const response = await this.personServices.addPerson(newPerson);
+    // Send a request to add the new person and await the response
+    const response = await this.personServices.addPerson(newPerson);
 
-      // Check if the response is an array, then push the items into personData
-      this.personServices.personData.push(response);
+    // Check if the response is an array, then push the items into personData
+    this.personServices.personData.push(response);
 
-      // Refresh teacherData after adding
-      await this.personServices.fetchPersonData();
-
-      // Display the list of teacher after adding
-      this.teacherList.displayTeacherList(this.personServices.personData);
-
-      // Directly stop loading spinner after response is received
-      stopLoadingSpinner();
-
-      // Show success toast message
-      showToast(MESSAGES.ADD_SUCCESS, ICONS.SUCCESS, true);
-    });
+    // Display the list of teacher after adding
+    this.teacherList.displayTeacherList(this.personServices.personData);
   }
 
   // Show the teacher modal with the given teacherData
@@ -151,30 +139,22 @@ export class TeacherPage {
     personId: string,
     updatedPerson: Person,
   ): Promise<void> {
-    delayAction(async () => {
-      // Edit the ad in the model
-      const response = await this.personServices.editPerson(
-        personId,
-        updatedPerson,
-      );
+    // Edit the ad in the model
+    const response = await this.personServices.editPerson(
+      personId,
+      updatedPerson,
+    );
 
-      // Find the edited ad in the personData array
-      const editedTeacher = this.personServices.personData.find(
-        (person) => person.id === personId,
-      );
+    // Find the edited ad in the personData array
+    const editedTeacher = this.personServices.personData.find(
+      (person) => person.id === personId,
+    );
 
-      // Update the edited teacher with the response data
-      editedTeacher && Object.assign(editedTeacher, response);
+    // Update the edited teacher with the response data
+    editedTeacher && Object.assign(editedTeacher, response);
 
-      // Display the list of teacher after edit
-      this.teacherList.displayTeacherList(this.personServices.personData);
-
-      // Directly stop loading spinner after response is received
-      stopLoadingSpinner();
-
-      // Show a success notification
-      showToast(MESSAGES.EDIT_SUCCESS, ICONS.SUCCESS, true);
-    });
+    // Display the list of teacher after edit
+    this.teacherList.displayTeacherList(this.personServices.personData);
   }
 
   /**
@@ -216,7 +196,7 @@ export class TeacherPage {
       response && stopLoadingSpinner();
 
       // Show a success notification
-      showToast(MESSAGES.DELETE_SUCCESS, ICONS.SUCCESS, true);
+      showToast(MESSAGES.DELETE_SUCCESS, ICONS.SUCCESS);
     });
   }
 
