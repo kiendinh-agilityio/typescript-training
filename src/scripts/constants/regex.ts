ff197139f1f72a271336ca772f3174decd140dd0
Kien Dinh <kien.dinh@asnet.com.vn>
2024-08-15 14:33:30 +0700

    feat: update for constants regex

--- src/scripts/constants/regex.ts
+++ src/scripts/constants/regex.ts
@@ -1,11 +1,9 @@
 export const REGEX = {
   // Check email have @ symbol, a string precedes it and the following string needs to contain a period, followed by 2-3 characters.
-  EMAIL:
-    /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}))$/,
+  EMAIL: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
 
   // Check regex for avatar url
-  AVATAR_URL:
-    /^(https?:\/\/)?(www\.)?[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})+(\.[a-zA-Z]{2,})?([/?].*)?$/,
+  AVATAR_URL: /^https?:\/\/.*\.(png|gif|webp|jpe?g)(\?.*)?$/i,
 
   // Check characters with strings or numbers between 6 and 20 characters in length
   CHARACTER: /[0-9a-zA-Z]{4,20}/,
