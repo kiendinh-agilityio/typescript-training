d3acb5d1f6039fbb0846f372bac40a3ec6e9eee1
Kien Dinh <kien.dinh@asnet.com.vn>
2024-08-23 17:50:00 +0700

    feat: update StudentList to inherit from PersonList

--- src/scripts/events/studentList.ts
+++ src/scripts/events/studentList.ts
@@ -5,51 +5,29 @@ import { generateListPerson, generateDetailStudent } from '@/templates';
 import { Person, PersonType } from '@/types';
 
 // Import constants
-import {
-  DISPLAY_CLASSES,
-  TITLE_MODAL,
-  PROFILE_PERSON,
-  ID_ELEMENTS,
-  CLASSES,
-  MESSAGES,
-  ICONS,
-  END_POINTS,
-  TIMES,
-} from '@/constants';
+import { DISPLAY_CLASSES, MESSAGES, END_POINTS, TIMES } from '@/constants';
 
 // Import utils
 import {
-  attachBlurEventHandlers,
   createToggleDropdown,
   createFilterClass,
-  displayFormErrors,
   displayFilterNoResult,
-  displayToastMessage,
-  generateModalPerson,
-  generateModalConfirm,
   modelStudent,
-  modelDeleteStudent,
   searchStudent,
-  trailingString,
   startLoadingSpinner,
-  stopLoadingSpinner,
-  validateForm,
 } from '@/utils';
 
 // Import class PersonServices
 import { PersonServices } from '@/services';
 
+// Import class PersonList
+import { PersonList } from '@/events';
+
 // Definition StudentList class
-export class StudentList {
+export class StudentList extends PersonList {
   tableStudent: HTMLElement;
   btnAddStudent: HTMLElement;
-  addStudentHandler: (person: Person) => void;
-  editStudentHandler: (personId: string, person: Person) => void;
   getDetailStudentHandler: (personId: number) => void;
-  confirmDeleteButton: HTMLElement;
-  cancelDeleteButton: HTMLElement;
-  closeDeleteModalButton: HTMLElement;
-  deleteStudentHandler: (personId: number) => void;
   btnSearchStudent: HTMLElement;
   inputSearchStudent: HTMLInputElement;
   clearSearchStudent: HTMLElement;
@@ -58,6 +36,7 @@ export class StudentList {
   selectedStudentId: string;
 
   constructor() {
+    super('modal-confirm-student');
     this.initElementsStudent();
     this.initEventListenersStudent();
     this.initializeSearchInput();
@@ -83,13 +62,6 @@ export class StudentList {
     this.clearSearchStudent = searchStudent.querySelector(
       '#clear-search-student',
     );
-
-    // Event listener for modal click
-    modelStudent.addEventListener('click', (event: MouseEvent) => {
-      if (event.target === modelStudent) {
-        this.closeModalHandler();
-      }
-    });
 
     // Event listener for button add click
     this.btnAddStudent.addEventListener('click', () => {
@@ -216,168 +188,7 @@ export class StudentList {
    * @param {Object} personData - The data of the student to be displayed in the modal.
    */
   showStudentModal(personData: Person): void {
-    const title = personData ? TITLE_MODAL.EDIT : TITLE_MODAL.ADD;
-    const modalStudentContent = generateModalPerson(personData, title, false);
-
-    // Set the modal's HTML content and display it
-    modelStudent.innerHTML = modalStudentContent;
-    modelStudent.style.display = DISPLAY_CLASSES.FLEX;
-
-    // Get references to the close button, cancel button, submit button, and the student form
-    const closeBtn = modelStudent.querySelector(
-      ID_ELEMENTS.CLOSE_MODAL,
-    ) as HTMLElement;
-    const cancelBtn = modelStudent.querySelector(
-      ID_ELEMENTS.BTN_CANCEL,
-    ) as HTMLElement;
-    const submitAddStudent = modelStudent.querySelector(
-      ID_ELEMENTS.BTN_SUBMIT,
-    ) as HTMLElement;
-    const formStudent = modelStudent.querySelector(
-      ID_ELEMENTS.FORM_PERSON,
-    ) as HTMLElement;
-
-    // Attach blur event handlers for form validation
-    attachBlurEventHandlers();
-
-    // Add event listeners for close button and cancel button clicks
-    closeBtn.addEventListener('click', this.closeModalHandler.bind(this));
-    cancelBtn.addEventListener('click', this.closeModalHandler.bind(this));
-
-    // Save old data if editing
-    const saveOldData = personData && personData;
-
-    // Initialize a flag to track whether changes have been made
-    let hasChange = false;
-
-    // Add event listeners for input changes to set the hasChange flag
-    const formInputs = modelStudent.querySelectorAll('input, select');
-    formInputs.forEach((input) => {
-      input.addEventListener('input', () => {
-        // Compare new values with old values
-        const name = trailingString(
-          (formStudent.querySelector(PROFILE_PERSON.NAME) as HTMLInputElement)
-            .value,
-        );
-
-        const avatarUrl = trailingString(
-          (formStudent.querySelector(PROFILE_PERSON.AVATAR) as HTMLInputElement)
-            .value,
-        );
-
-        const email = trailingString(
-          (formStudent.querySelector(PROFILE_PERSON.EMAIL) as HTMLInputElement)
-            .value,
-        );
-
-        const className = (
-          formStudent.querySelector(PROFILE_PERSON.CLASS) as HTMLSelectElement
-        ).value;
-
-        const gender = (
-          formStudent.querySelector(PROFILE_PERSON.GENDER) as HTMLSelectElement
-        ).value;
-
-        // Check if the new data is different from the old data
-        hasChange = saveOldData
-          ? name !== saveOldData.name ||
-            avatarUrl !== saveOldData.avatarUrl ||
-            email !== saveOldData.email ||
-            className !== saveOldData.className ||
-            gender !== saveOldData.gender
-          : true;
-
-        // Enable the submit button when changes are made and the modal is "Edit student"
-        if (title === TITLE_MODAL.EDIT && hasChange) {
-          submitAddStudent.removeAttribute(DISPLAY_CLASSES.DISABLED);
-          submitAddStudent.classList.remove(CLASSES.BUTTON_DISABLE);
-        } else if (!hasChange) {
-          submitAddStudent.setAttribute(
-            DISPLAY_CLASSES.DISABLED,
-            DISPLAY_CLASSES.DISABLED,
-          );
-
-          submitAddStudent.classList.add(CLASSES.BUTTON_DISABLE);
-        }
-      });
-    });
-
-    // Handle the event of submitting the form
-    submitAddStudent.addEventListener('click', async () => {
-      const name = trailingString(
-        (formStudent.querySelector(PROFILE_PERSON.NAME) as HTMLInputElement)
-          .value,
-      );
-
-      const avatarUrl = trailingString(
-        (formStudent.querySelector(PROFILE_PERSON.AVATAR) as HTMLInputElement)
-          .value,
-      );
-
-      const email = trailingString(
-        (formStudent.querySelector(PROFILE_PERSON.EMAIL) as HTMLInputElement)
-          .value,
-      );
-
-      const className = (
-        formStudent.querySelector(PROFILE_PERSON.CLASS) as HTMLSelectElement
-      ).value;
-
-      const gender = (
-        formStudent.querySelector(PROFILE_PERSON.GENDER) as HTMLSelectElement
-      ).value;
-
-      const person: Person = {
-        id: '',
-        name,
-        avatarUrl,
-        email,
-        className,
-        gender,
-      };
-
-      // Clear previous errors
-      this.clearErrorMessageForm();
-
-      // Validate the student and show errors
-      const errors = validateForm(person, PersonType.Student);
-
-      if (Object.entries(errors).length > 0) {
-        displayFormErrors(errors);
-      } else if (hasChange) {
-        // Close the modal
-        this.closeModalHandler();
-
-        // Start the spinner
-        startLoadingSpinner();
-
-        // Determine whether this operation is an edit or an add
-        const isEditStudent = !!personData;
-
-        // Perform the appropriate action: edit or add student
-        await (isEditStudent
-          ? this.editStudentHandler(personData.id, person)
-          : this.addStudentHandler(person));
-
-        // Stop the spinner
-        stopLoadingSpinner();
-
-        // Show success toast message add or edit
-        displayToastMessage(
-          isEditStudent ? MESSAGES.EDIT_SUCCESS : MESSAGES.ADD_SUCCESS,
-          ICONS.SUCCESS,
-        );
-      }
-    });
-
-    if (title === TITLE_MODAL.EDIT) {
-      submitAddStudent.setAttribute(
-        DISPLAY_CLASSES.DISABLED,
-        DISPLAY_CLASSES.DISABLED,
-      );
-
-      submitAddStudent.classList.add(CLASSES.BUTTON_DISABLE);
-    }
+    this.showPersonModal(personData, modelStudent, false);
   }
 
   /**
@@ -385,24 +196,7 @@ export class StudentList {
    * @param {Function} handler - The handler function for adding student.
    */
   bindAddStudent(handler: (person: Person) => void): void {
-    this.addStudentHandler = handler;
-  }
-
-  // Close the modal
-  closeModalHandler(): void {
-    modelStudent.style.display = DISPLAY_CLASSES.HIDDEN;
-  }
-
-  /**
-   * Clear Error Message for Form
-   */
-  clearErrorMessageForm(): void {
-    const errorFields = ['name', 'avatarUrl', 'email', 'className', 'gender'];
-
-    errorFields.forEach((field: string) => {
-      const errorElement = modelStudent.querySelector(`#${field}-error`)!;
-      errorElement.textContent = '';
-    });
+    this.bindAddHandler(handler);
   }
 
   /**
@@ -410,7 +204,7 @@ export class StudentList {
    * @param {Function} handler - The handler function for editing student.
    */
   bindEditStudent(handler: (personId: string, person: Person) => void): void {
-    this.editStudentHandler = handler;
+    this.bindEditHandler(handler);
   }
 
   // Bind the handler for getting detail of an student
@@ -420,52 +214,7 @@ export class StudentList {
 
   // Bind the delete ad handler to the table element
   bindDeleteStudent(handler: (personId: number) => void): void {
-    this.deleteStudentHandler = handler;
-  }
-
-  // Show confirm modal
-  showConfirmModal(personId: number): void {
-    modelDeleteStudent.innerHTML = generateModalConfirm();
-
-    // Get button
-    const confirmDeleteButton = modelDeleteStudent.querySelector(
-      '#confirm-btn',
-    ) as HTMLElement;
-    const cancelDeleteButton = modelDeleteStudent.querySelector(
-      '#cancel-btn',
-    ) as HTMLElement;
-    const closeDeleteModalButton = modelDeleteStudent.querySelector(
-      '#close-modal-confirm',
-    ) as HTMLElement;
-
-    // Set data-id attribute for confirm button
-    confirmDeleteButton.setAttribute('data-id', personId.toString());
-
-    // Add event listeners
-    confirmDeleteButton.addEventListener('click', () => {
-      const personId = parseInt(confirmDeleteButton.getAttribute('data-id')!);
-
-      this.hideDeleteModal();
-      this.deleteStudentHandler(personId);
-    });
-
-    cancelDeleteButton.addEventListener(
-      'click',
-      this.hideDeleteModal.bind(this),
-    );
-
-    closeDeleteModalButton.addEventListener(
-      'click',
-      this.hideDeleteModal.bind(this),
-    );
-
-    // Show modal confirm
-    modelDeleteStudent.style.display = DISPLAY_CLASSES.FLEX;
-  }
-
-  // Hide modal confirm
-  hideDeleteModal(): void {
-    modelDeleteStudent.style.display = DISPLAY_CLASSES.HIDDEN;
+    this.bindDeleteHandler(handler);
   }
 
   // Initialize the search input and its event listeners
